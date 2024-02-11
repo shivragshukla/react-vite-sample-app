cc7e7cb6f6a42481910543f1fcb843ace799a6ec
Shivrag <shivragshukla001@gmail.com>
2024-02-11 21:37:18 +0530

    Added: About us page

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Layout from './components/Layout.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ <Layout /> }></Route>
+        <Route path='/' element={ <Layout /> }>
+          <Route index element={ <Home />} />
+          <Route path='about' element= {<About />} />
+        </Route>
       </Routes>    
     </BrowserRouter>
   </React.StrictMode>,
---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <>
+            <h1 className='title'>About</h1>
+            <p>This is a about us page</p>
+        </>
+    )
+    
+}
+export default About
