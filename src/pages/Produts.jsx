860f1cfb4d42c9950d6e5d8bf0482cac41118043
Shivrag <shivragshukla001@gmail.com>
2024-02-11 22:56:34 +0530

    Added: dummy product api

--- src/pages/Produts.jsx
+++ src/pages/Produts.jsx
@@ -1,8 +1,37 @@
+import { useState, useEffect } from "react";
+
 function Products() {
+
+    const [products, setProducts] = useState([]);
+
+
+    async function fetchProducts() {
+        const resposne = await fetch(`https://dummyjson.com/products?limit=10`);
+        const data = await resposne.json()
+        setProducts(data.products)
+    }
+    useEffect(()=>{
+        fetchProducts()
+
+    }, [])
+    
     return (
         <>
             <h1 className='title'>Products</h1>
             <p>This is a products page</p>
+            <div className="products">
+                {
+                    products && products.length > 0
+                    ?   products.map(product => 
+                            <div className="item" key={product.id}>
+                                <img className="image" src={product.thumbnail} alt={product.title} />
+                                <h3 className="title">{ product.title }</h3>
+                            </div>
+                        )
+                    : <p>No data found</p>
+                }
+
+            </div>
         </>
     );
 }
